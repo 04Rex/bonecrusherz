cff2cd9fac17eb7d0ac9e8532279db693afff760
04Rex <tlc2004@outlook.com>
2023-08-15 16:35:46 +0100

    Full support for @vercel/speed-insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,22 @@
 	import Header from './Header.svelte';
 	import './styles.css';
 	import { inject } from '@vercel/analytics';
+	import { webVitals } from '$lib/vitals';
+	import { browser } from '$app/env';
+	import { page } from '$app/stores';
+	import '../app.css';
+
+	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+
 	inject({mode: "production"});
+
+	$: if (browser && analyticsId) {
+		webVitals({
+			path: $page.url.pathname,
+			params: $page.params,
+			analyticsId
+		})
+	}
 </script>
 
 <div class="app">
